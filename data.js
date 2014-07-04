a4223d7b9ad6e780517cd049e853f7af0bba6c30
Hsiaoming Yang <me@lepture.com>
2014-07-04 22:14:28 +0800

    Add vimeo and android

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
 data = [
-  {title: "Invision", colors: ['#54CDBA', '#D95685', '#7CD0E5', '#F8B35C', '#7CC041']}
+  {title: "Invision", colors: ['#54CDBA', '#D95685', '#7CD0E5', '#F8B35C', '#7CC041']},
+  {title: "Vimeo", colors: ['#44BBFF', '#F7B42C', '#AAD450', '#F27490']},
+  {title: "Android", colors: ['#33B5E5', '#9933CC', '#99CC00', '#FFBB33', '#FF4444']}
 ];
